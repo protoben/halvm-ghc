3d929939005235b82d3344b27d15931fabb357a8
Adam Wick <awick@galois.com>
2013-05-29 17:03:49 -0700

    More miscellaneous header mods.

---
+++ rts/xen/include/poll.h
@@ -0,0 +1,19 @@
+#ifndef HALVM_POLL_H
+#define HALVM_POLL_H
+
+typedef unsigned long int nfds_t;
+
+struct pollfd {
+  int        fd;
+  short int events;
+  short int revents;
+};
+
+#define POLLIN          0x001
+#define POLLOUT         0x004
+#define POLLERR         0x008
+#define POLLHUP         0x010
+
+int poll(struct pollfd fds[], nfds_t nfds, int timeout);
+
+#endif
--- rts/xen/include/time.h
+++ rts/xen/include/time.h
@@ -68,25 +68,6 @@ struct tm {
 	__const char *tm_zone;
 };
 
-struct rusage {
-	struct timeval ru_utime; /* user time used */
-	struct timeval ru_stime; /* system time used */
-	long   ru_maxrss;        /* maximum resident set size */
-	long   ru_ixrss;         /* integral shared memory size */
-	long   ru_idrss;         /* integral unshared data size */
-	long   ru_isrss;         /* integral unshared stack size */
-	long   ru_minflt;        /* page reclaims */
-	long   ru_majflt;        /* page faults */
-	long   ru_nswap;         /* swaps */
-	long   ru_inblock;       /* block input operations */
-	long   ru_oublock;       /* block output operations */
-	long   ru_msgsnd;        /* messages sent */
-	long   ru_msgrcv;        /* messages received */
-	long   ru_nsignals;      /* signals received */
-	long   ru_nvcsw;         /* voluntary context switches */
-	long   ru_nivcsw;        /* involuntary context switches */
-};
-
 /* prototypes */
 void       init_time(void);
 int        gettimeofday(struct timeval *tv,void *ignore);
@@ -94,7 +75,6 @@ u32        block_domain(u32 millisecs);
 u64        monotonic_clock(void);
 struct tm *localtime_r(const time_t *timep, struct tm *result);
 struct tm *gmtime_r(const time_t *timep, struct tm *result);
-int		   getrusage(int who, struct rusage *usage);
 time_t     mktime(struct tm *tm);
 time_t     time(time_t *t);
 char      *ctime_r(const time_t *timep, char *buf);
