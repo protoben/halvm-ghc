06a443945e07ca29bcd0ddf67ca17e93acd4b313
Adam Wick <awick@galois.com>
2013-09-04 16:28:35 -0700

    Tweak the minimal IO manager.

--- rts/xen/iomanager.c
+++ rts/xen/iomanager.c
@@ -1,8 +1,8 @@
 #include "Rts.h"
 #include "Prelude.h"
 
-static int controlFd = 0;
-static int wakeupFd = 0;
+static int controlFd = -1;
+static int wakeupFd = -1;
 
 void setIOManagerControlFd(int fd)
 {
@@ -25,8 +25,9 @@ void ioManagerWakeup(void)
 #ifdef THREADED_RTS
 void ioManagerDie(void)
 {
-  printf("ioManagerDie()\n");
-  // FIXME
+  if(controlFd >= 0) {
+    printf("ioManagerDie: controlFd: %d wakeupFd: %d\n", controlFd, wakeupFd);
+  }
 }
 
 void ioManagerStart(void)
@@ -34,7 +35,6 @@ void ioManagerStart(void)
   Capability *cap;
 
   if(controlFd < 0 || wakeupFd < 0) {
-    printf("starting ioManager capability?\n");
     cap = rts_lock();
     rts_evalIO(&cap, &base_GHCziConcziIO_ensureIOManagerIsRunning_closure,NULL);
     rts_unlock(cap);
